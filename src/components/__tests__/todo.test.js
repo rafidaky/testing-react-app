7f929e8293ec4b80d090a7f0fe57095d12358fd2
Rafida <rafidakn@gmail.com>
2023-02-12 21:53:44 +0100

    snapshot & tests

---
+++ src/components/__tests__/todo.test.js
@@ -0,0 +1,32 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import ToDo from "../todo";
+
+afterEach(() => {
+  cleanup();
+});
+
+test("should render non-completed todo component", () => {
+  const todo = { id: 1, title: "wash dishes", completed: false };
+  render(<ToDo todo={todo} />);
+  const todoElement = screen.getByTestId("todo-1");
+  expect(todoElement).toBeInTheDocument();
+  expect(todoElement).toHaveTextContent("wash dishes");
+  expect(todoElement).not.toContainHTML("<strike>");
+});
+
+test("should render completed todo component", () => {
+  const todo = { id: 2, title: "wash car", completed: true };
+  render(<ToDo todo={todo} />);
+  const todoElement = screen.getByTestId("todo-2");
+  expect(todoElement).toBeInTheDocument();
+  expect(todoElement).toHaveTextContent("wash car");
+  expect(todoElement).toContainHTML("strike");
+});
+
+test("matches snapshot", () => {
+  const todo = { id: 1, title: "wash dishes", completed: false };
+  const tree = renderer.create(<ToDo todo={todo} />).toJSON();
+  //snapshots!
+  expect(tree).toMatchSnapshot();
+});
